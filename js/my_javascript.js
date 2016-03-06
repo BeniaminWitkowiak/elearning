da5c89841c75909c685827f4948d924211dd98d8
BeniaminWitkowiak <ben_as@o2.pl>
2016-03-06 13:50:22 +0100

    Setup

---
+++ js/my_javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	$('body').find('.wysun').slideDown();
+	
+	
+	
+	
+	
+	
+	$('body').on('mouseover', '.for_hover', function () {
+        $(this).find('.details').slideDown();
+		}).on('mouseleave', '.for_hover', function () {
+        $(this).find('.details').slideUp();
+	});
+	/*
+		$('body').on('click', '.check_button', function () {
+		if ($(this).closest('body').find('checkboxG11').is(':checked')) {
+		alert("CHECKED !!!!")
+		} else {
+		alert('Unchecked');
+		}
+	});*/
+	$('body').on('click', '.check_button', function () {
+	if ($('body').find('input.css-checkbox').is(':checked')) {
+		alert('checked');
+		}
+		else{
+			alert('unchecked');
+			}
+
+
+	});
+});
+
