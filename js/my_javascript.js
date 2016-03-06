1fea69281f33d1917fd30dfb3cba1992b3aff40e
BeniaminWitkowiak <ben_as@o2.pl>
2016-03-07 00:08:58 +0100

    Added: TrueOrFalse.html

--- js/my_javascript.js
+++ js/my_javascript.js
@@ -17,23 +17,42 @@ $(document).ready(function () {
 		}
 	});*/
 	$('body').on('click', '.check_button', function () {
-	if ($('body').find('input.css-checkbox').is(':checked')) {
-		alert('checked');
-	}else{
-		alert('unchecked');
-	}
+		if ($('body').find('input.css-checkbox').is(':checked')) {
+			alert('checked');
+			}else{
+			alert('unchecked');
+		}
 	});
 	
 	/*Akcja po zaznaczeniu odpowiedzi w zadaniu z jedną poprawną odpowiedzią*/
 	$('body').on('click', '.incorrect_answer', 
 	function () {
-			alert('incorrect');
-		});
-			
+		alert('incorrect');
+	});
+	
 	$('body').on('click', '.correct_answer', 
-		function () {
-			alert('correct');
-		});
+	function () {
+		alert('correct');
+	});
+	
+	
+	
+	
+	$('body').on('click', '.true', function () {
+		$('.grey_background').css("display", "block");
+		$('.incorrect_message').css("display", "block");
+	});
+	$('body').on('click', '.false', function () {
+		$('.grey_background').css("display", "block");
+		$('.correct_message').css("display", "block");
+	});
+	
+	
+	$('body').on('click', '.grey_background', function () {
+		$(this).slideUp();
+		$('.correct_message').css("display", "none");
+		$('.incorrect_message').css("display", "none");
+	});
 	
 });
 
